345aa832dfcc9d14febf5cb1f0ba990b069973cb
dilly <65288602+dillytin@users.noreply.github.com>
2022-04-02 16:41:10 +0900

    Revert "End the day"
    This reverts commit 9f8742066ba9efaae51a8cec229238e7c23a8ec6.

--- crawl-ref/source/util/gen-apt.pl
+++ crawl-ref/source/util/gen-apt.pl
@@ -130,7 +130,6 @@ sub aptitude_table
         next if $sp eq 'Lava Orc';
         next if $sp eq 'Centaur';
         next if $sp eq 'Halfling';
-        next if $sp eq 'Mayflytaur';
 
         my $line = '';
         $line .= fix_draco_species($sp, \$seen_draconian_length);
